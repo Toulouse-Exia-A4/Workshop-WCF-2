bceb5077bce101bd875b41897fbab05d4e30f647
Simon Tod <simon.todeschini@viacesi.fr>
2018-06-12 16:17:54 +0200

    corrected structure

---
+++ eXia_A4L_WS_SECUWCF_SERVER/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eXia_A4L_WS_SECUWCF_SERVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
