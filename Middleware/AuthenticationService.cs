0d253b1702d4ea63a5e5127448b970dfc1cbc1cd
Victor Schirmann <victorino_s@outlook.com>
2018-06-13 10:54:30 +0200

    Middleware implementation

---
+++ Middleware/AuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Middleware
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "AuthenticationService" à la fois dans le code et le fichier de configuration.
+    public class AuthenticationService : IAuthenticationService
+    {
+        public bool Authenticate(string username, string password)
+        {
+            var client = new CADService.CAD_InterfaceClient();
+            return client.Authenticate(username, password);
+        }
+
+        public bool CheckExistence(string username)
+        {
+            var client = new CADService.CAD_InterfaceClient();
+            return client.CheckExistence(username);
+        }
+    }
+}
